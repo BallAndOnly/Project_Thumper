27d451f52aafc183ce333a163b3377165ca7eecf
Balls <loommoontrikun2@gmail.com>
2022-11-17 11:54:16 +0700

    Initial commit

---
+++ Assets/Scripts/Player Controller/PlayerCam.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    public Transform CamPos;
+
+    private void Start()
+    {
+        //Make cursor gone
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        transform.position = CamPos.position;
+
+        //Look around
+        float mouseX = Input.GetAxis("Mouse Y") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxis("Mouse X") * Time.deltaTime * sensY;
+
+        xRotation -= mouseX;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //Cam clamp to make sure you don't spin 360 vertically
+        yRotation += mouseY;
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
---
+++ Assets/Scripts/Player Controller/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    float hInput;
+    float vInput;
+
+
+
+}
